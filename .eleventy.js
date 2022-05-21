8e259b228b4e2783c656a5021f0e9def19503563
Niels van Rongen <github@niels.im>
2022-05-21 22:24:55 +0200

    New site

--- .eleventy.js
+++ .eleventy.js
@@ -1,78 +1,60 @@
-const { DateTime } = require("luxon");
-const pluginRss = require("@11ty/eleventy-plugin-rss");
-const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const rssPlugin = require('@11ty/eleventy-plugin-rss');
 
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(pluginRss);
-  eleventyConfig.addPlugin(pluginSyntaxHighlight);
-  eleventyConfig.setDataDeepMerge(true);
+// Filters
+const dateFilter = require('./src/filters/date-filter.js');
+const w3DateFilter = require('./src/filters/w3-date-filter.js');
 
-  eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
+// Transforms
+const htmlMinTransform = require('./src/transforms/html-min-transform.js');
 
-  eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
+// Create a helpful production flag
+const isProduction = process.env.NODE_ENV === 'production';
+
+// Utils
+const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
+
+module.exports = config => {
+  // Add filters
+  config.addFilter('dateFilter', dateFilter);
+  config.addFilter('w3DateFilter', w3DateFilter);
+  config.addPassthroughCopy('./src/images/');
+  config.addPassthroughCopy('./src/fonts/');
+  // Plugins
+  config.addPlugin(rssPlugin);
+
+  // Returns articles items, sorted by display order
+  // config.addCollection('articles', collection => {
+  //   return sortByDisplayOrder(collection.getFilteredByGlob('./src/articles/*.md'));
+  // });
+
+  // Returns a collection of blog posts in reverse date order
+  config.addCollection('blog', collection => {
+    return [...collection.getFilteredByGlob('./src/posts/*.md')].reverse();
   });
 
-  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toFormat('dd-LL-yyyy');
-  });
+  // Only minify HTML if we are in production because it slows builds _right_ down
+  if (isProduction) {
+    config.addTransform('htmlmin', htmlMinTransform);
+  }
 
-  // Get the first `n` elements of a collection.
-  eleventyConfig.addFilter("head", (array, n) => {
-    if( n < 0 ) {
-      return array.slice(n);
-    }
+  // // Returns aricles, sorted by display order then filtered by featured
+  // config.addCollection('featuredArticles', collection => {
+  //   return sortByDisplayOrder(collection.getFilteredByGlob('./src/articles/*.md')).filter(
+  //     x => x.data.featured
+  //   );
+  // });
 
-    return array.slice(0, n);
-  });
+  // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
+  config.setUseGitIgnore(false);
 
-  eleventyConfig.addCollection("tagList", require("./_11ty/getTagList"));
-
-  eleventyConfig.addPassthroughCopy("img");
-  eleventyConfig.addPassthroughCopy("css");
-
-  /* Markdown Plugins */
-  let markdownIt = require("markdown-it");
-  let markdownItAnchor = require("markdown-it-anchor");
-  let options = {
-    html: true,
-    breaks: true,
-    linkify: true
-  };
-  let opts = {
-    permalink: true,
-    permalinkClass: "direct-link",
-    permalinkSymbol: "#"
-  };
-
-  eleventyConfig.setLibrary("md", markdownIt(options)
-    .use(markdownItAnchor, opts)
-  );
-
+  // Return your Object options:
   return {
-    templateFormats: [
-      "md",
-      "njk",
-      "html",
-      "liquid"
-    ],
-
-    // If your site lives in a different subdirectory, change this.
-    // Leading or trailing slashes are all normalized away, so don’t worry about it.
-    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
-    // This is only used for URLs (it does not affect your file structure)
-    pathPrefix: "",
-
-    markdownTemplateEngine: "liquid",
-    htmlTemplateEngine: "njk",
-    dataTemplateEngine: "njk",
-    passthroughFileCopy: true,
+    markdownTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
     dir: {
-      input: ".",
-      includes: "_includes",
-      data: "_data",
-      output: "_site"
+      input: 'src',
+      output: 'dist'
     }
   };
 };
---
+++ src/index.md
@@ -0,0 +1,6 @@
+---
+title: "Hello I'm Niels"
+layout: "layouts/home.html"
+metaDesc: 'The website of Niels van Rongen.'
+---
+A front-end developer from Leiden, the Netherlands. This is my personal website. Currently I'm working on building accessible websites by using HTML, JavaScript and CSS. I position myself somewhere between design, marketing and development. I make sure a website is accessible, looks good, has excellent performance and is SEO optimized.
